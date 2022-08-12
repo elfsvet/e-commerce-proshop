f1cc637e5d7ad38f899fd2b00e30267f150169db
elfsvet <stepanmatysik@gmail.com>
2022-08-12 16:17:22 -0400

    created a product details page with information and add to cart button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 const ProductScreen = () => {
-  return (
-    <div>ProductScreen</div>
-  )
+    const navigate = useNavigate()
+    const params = useParams()
+    const product = products.find(p => p._id === params.id)
+
+    return (
+        <>
+            <Link className='btn btn-dark my-3' to='/'>Go Back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
+                            />
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Price: ${product.price}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Description: ${product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Status:
+                                    </Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            {/* we use the class name here instead of button btn-block because it didn't work in the new version of the bootstrap */}
+                            <ListGroupItem className="d-grid gap-2">
+                                <Button type='button' disabled={product.countInStock === 0}>
+                                    Add To Cart
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    )
 }
 
 export default ProductScreen
