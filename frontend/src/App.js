22aa21dffe9a3c9f001c0c9dd9096fbf1e7bcc03
elfsvet <stepanmatysik@gmail.com>
2022-08-12 14:19:08 -0400

    set the products cards with rating and some css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 import { Container } from 'react-bootstrap'
 const App = () => {
   return (
@@ -7,8 +8,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-
-          <h1>Welcome To ProShop</h1>
+         <HomeScreen/>
         </Container>
       </main>
       <Footer />
