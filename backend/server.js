5835e418e86b0b5eaaca44ced5bdbcd9372633e3
elfsvet <stepanmatysik@gmail.com>
2022-08-12 20:09:45 -0400

    change required to import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
 const app = express();
 
 dotenv.config()
@@ -22,4 +23,4 @@ const PORT = process.env.PORT || 5000
 app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`)
-)
+    )
