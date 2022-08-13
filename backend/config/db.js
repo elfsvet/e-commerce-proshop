1b060a72fe8da70702e6c52c042d76eaa04eceb5
elfsvet <stepanmatysik@gmail.com>
2022-08-12 21:49:21 -0400

    created seeder and destroyer of the seeded data

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-        // no options needed on the mongoose 6 and up. its included
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        // useNewUrlParser , useUnifiedTopology , useFindAndModify , and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser , useUnifiedTopology , and useCreateIndex are true , and useFindAndModify is false . no options needed on the mongoose 6 and up. its included
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
 
