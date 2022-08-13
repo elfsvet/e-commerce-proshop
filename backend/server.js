0a049b5159fdc528c5b9f8ff50ab789699ac6095
elfsvet <stepanmatysik@gmail.com>
2022-08-12 22:49:37 -0400

    created middleware to handle error and not found product

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,31 @@ import colors from 'colors'
 
 import productRoutes from './routes/productRoutes.js'
 
+import { notFound, errorHandler} from './middleware/errorMiddleware.js'
+
 dotenv.config()
 
 connectDB()
 
 const app = express();
 
+/*          CUSTOM MIDDLEWARE
+ app.use((req, res, next) => {
+    console.log(req.originalUrl)
+    next();
+}) */
+
 app.get('/', (req, res) => {
     res.send('API is running....')
 })
 
 app.use('/api/products', productRoutes)
+
+
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000
 
