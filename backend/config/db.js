2f38b26b10f4d4d075d7d1871fa5e2596951470b
elfsvet <stepanmatysik@gmail.com>
2022-08-12 20:54:06 -0400

    added color styles to messages in CLI

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ const connectDB = async () => {
     try {
         // no options needed on the mongoose 6 and up. its included
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
 
-        console.error(`Error: ${error.message}`)
+        console.error(`Error: ${error.message}`.red.underline.bold)
         process.exit(1)
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js'
+// to use for colorful messages in CLI
+import colors from 'colors'
 
 dotenv.config()
 
@@ -25,5 +27,5 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`.yellow.bold)
 )
