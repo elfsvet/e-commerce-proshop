80744e056f9ce90541dd97f1b670c6ed2294be4a
elfsvet <stepanmatysik@gmail.com>
2022-08-13 23:37:10 -0400

    add to cart Functionality and he fixed the issue with 0qty in location.search

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
+
+
 
 const CartScreen = () => {
+  const navigate = useNavigate()
+  const params = useParams()
+  const productId = params.id
+  console.log(productId)
+  // no more location history or match. for location use hook useLocation
+  const location = useLocation()
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+  const dispatch = useDispatch()
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  },[dispatch, productId, qty])
   return (
+
     <div>
-        CART
+      CART
     </div>
   )
 }
