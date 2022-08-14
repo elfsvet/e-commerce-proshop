989d628980db0f2ccfd936db4c535df46ece5f0b
elfsvet <stepanmatysik@gmail.com>
2022-08-14 13:20:52 -0400

    created middleware to getUserProfile and routes to get the api/profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ const authUser = asyncHandler(async (req, res) => {
     // })
     const user = await User.findOne({ email })
 
-    if(user && (await user.matchPassword(password))){
+    if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
@@ -30,4 +30,27 @@ const authUser = asyncHandler(async (req, res) => {
 
 })
 
-export { authUser }
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    // using out middleware and assigning it to req.user
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        // returns the users data
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+
+    } else {
+        res.status(401)
+        throw new Error('User not found')
+   }
+})
+
+export { authUser, getUserProfile }
