7ed8f6114af8ec12b61dfde2f52c71f0640299cb
elfsvet <stepanmatysik@gmail.com>
2022-08-15 23:32:56 -0400

    still need help with CREATE ORDER. Done with get order by ID endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,10 @@
 // to not write the error logic with try/catch use this
+import { response } from 'express'
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
 // @desc    Create new order
-// @route   POST /api/products
+// @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
@@ -35,6 +36,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export {
-    addOrderItems
-}
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById}
