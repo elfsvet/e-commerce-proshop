95ad0ed2743c1580a718a7bd5a470eeae33df11a
elfsvet <stepanmatysik@gmail.com>
2022-09-02 00:06:49 -0400

    created back end for update a product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,22 @@
 import express from 'express';
 const router = express.Router();
-import { deleteProduct, getProductById, getProducts } from '../controllers/productController.js'
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from '../controllers/productController.js';
 // we cleaned up it to controllers productController.js
-import {protect, admin} from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js';
 //we can do this router.get('/',getProducts) but I would love to try this:
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 // we also try a different route router.get('/:id', getProductById)
-router.route('/:id').get(getProductById).delete(protect,admin,deleteProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
-
-export default router
+export default router;
