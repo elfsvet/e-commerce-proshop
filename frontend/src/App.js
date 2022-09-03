94510027f7b268ee7f413787e302780b22d41a48
elfsvet <stepanmatysik@gmail.com>
2022-09-02 21:16:42 -0400

    added the screen for editing the product. Would need to update how the update will work eventually

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -39,6 +40,7 @@ const App = () => {
             <Route path='/admin/userlist' element={<UserListScreen />} />
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
             <Route path='/admin/productlist' element={<ProductListScreen />} />
+            <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
 
             <Route path='/' element={<HomeScreen />} />
           </Routes>
