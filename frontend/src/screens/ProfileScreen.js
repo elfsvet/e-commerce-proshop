545a13242a64d7812d24fb1ff2ffa267ba06f08a
elfsvet <stepanmatysik@gmail.com>
2022-09-04 17:51:20 -0400

    fixed details for user order

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -156,7 +156,7 @@ const ProfileScreen = () => {
                     )}
                   </td>
                   <td>
-                    <LinkContainer to={`/orders/${order._id}`}>
+                    <LinkContainer to={`/order/${order._id}`}>
                       <Button className='btn-sm' variant='light'>
                         Details
                       </Button>
