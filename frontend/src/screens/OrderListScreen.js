5781d7f485efb9419cddddc9e4e18f4a728f52af
elfsvet <stepanmatysik@gmail.com>
2022-09-04 18:52:43 -0400

    fix orders for admin too

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -68,7 +68,7 @@ const OrderListScreen = () => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/orders/${order._id}`}>
+                  <LinkContainer to={`/order/${order._id}`}>
                     <Button variant='light' className='btn-sm'>
                       Details
                     </Button>
