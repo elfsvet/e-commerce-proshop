a0005d92bd81da1ce1618895462d5ceb6931c79e
elfsvet <stepanmatysik@gmail.com>
2022-09-04 16:30:10 -0400

    pushing to heroku

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,6 @@ if (process.env.NODE_ENV === 'development') {
 // middleware to accept json in the body request
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running....');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -45,6 +41,17 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 // create a folder static is like this
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,9 @@ import Message from '../components/Message';
 import Loader from '../components/Loader';
 import Paginate from '../components/Paginate';
 import ProductCarousel from '../components/ProductCarousel';
+import Meta from '../components/Meta';
 import { listProducts } from '../actions/productActions';
+import { Link } from 'react-router-dom';
 
 const HomeScreen = () => {
   const params = useParams();
@@ -31,7 +33,8 @@ const HomeScreen = () => {
 
   return (
     <>
-      {!keyword && <ProductCarousel />}
+      <Meta />
+      {!keyword ? <ProductCarousel /> : <Link to='/' className='btn btn-light'>Go Back</Link>}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
