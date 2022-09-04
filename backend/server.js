7eaf4307e42e6a38a9ca86fd638c8aeb0d6278ac
elfsvet <stepanmatysik@gmail.com>
2022-09-03 21:50:40 -0400

    added back end logic to upload an image file for using for picture for a product, using multer package

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,49 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
+import path from 'path';
+import express, { application } from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 // to use for colorful messages in CLI
-import colors from 'colors'
+import colors from 'colors';
 // to make our routes work in server we need to import them
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 // middleware to accept json in the body request
-app.use(express.json())
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+  res.send('API is running....');
+});
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 // when we ready to make our payment we will hit this route and fetch this client id
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
-)
+);
+// current directory. Not available if we using ES modules its only available if we using common js require syntax
+// to mimic this we will use this:
+const __dirname = path.resolve();
+// create a folder static is like this
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
@@ -44,4 +51,4 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
       .yellow.bold
   )
-)
+);
